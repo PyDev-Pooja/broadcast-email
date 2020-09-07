9d42cd909f506013c011d3d70a8336812a8ecb80
Pooja Prakash <p80781@gmail.com>
2020-09-07 20:14:34 +0530

    Create broadcast.py

---
+++ broadcast.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import smtplib
+a = pd.read_excel("Email.xlsx")
+emails = a['Emails'].values
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login("<SenderAddress>", "<password">)
+msg = "Hello this is a broadcast email using python"
+subject = "Hello wwlcome to the world of python"
+body = "Subject: {}\n\n{}".format(subject,msg)
+for email in emails:
+    server.sendmail(SenderAddress, email, body)
+server.quit()
